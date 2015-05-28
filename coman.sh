71e3a81c4c1162f42dea621ef67d450e2867fb91
Christian <christian.kautzner@gmail.com>
2015-05-28 12:09:37 +0200

    added todo / structure

--- coman.sh
+++ coman.sh
@@ -6,6 +6,14 @@ width=49
 divider===============================
 divider=$divider$divider
 
+# structure:
+# print csv files in current dir, ask for seelction
+# read first line of csv for table-header information
+# use tput columns for terminal width and fancy layout
+# implement add, remove, edit, sort functions
+# add export to html, pdf and md?
+# dynamic web view?
+
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 
 printf '%4s | %20s | %10s | %3s\n' "Year" "Title" "Genre" "Rating"
